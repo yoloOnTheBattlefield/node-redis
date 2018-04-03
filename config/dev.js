94e5ddc50702d0d7d9a684faa25c5285ce317de7
Cristian Florea <cristianflorea@Cristians-MacBook-Pro.local>
2018-04-03 20:33:14 +0100

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI: "mongodb://cristian:root@ds231549.mlab.com:31549/blog-app-dev",
+  cookieKey: "123123123"
+};
