8f9a7ca66593ae5815cba697124c2922091ebf13
Cristian Florea <cristianflorea@Cristians-MacBook-Pro.local>
2018-04-04 20:23:34 +0100

    added redis instance

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -13,13 +13,32 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    try {
+      const redis = require("redis");
+      const util = require("util");
+      const redisUrl = "redis://127.0.0.1:6379";
+      const client = redis.createClient(redisUrl);
+      client.get = util.promisify(client.get);
 
-    res.send(blogs);
+      // Do we have any cached data in redis related to this query?
+      const cachedBlogs = await client.get(req.user.id);
+      //if yes, respond to the request
+      if (cachedBlogs) {
+        return res.send(JSON.parse(cachedBlogs));
+      }
+      //else respond to the request and cache the data
+      const blogs = await Blog.find({ _user: req.user.id });
+
+      console.log("SERVING FROM MONGO");
+      res.send(blogs);
+      client.set(req.user.id, JSON.stringify(blogs));
+    } catch (err) {
+      console.log(err);
+    }
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
