ac5dddad00c27291511c483063931cb984b554c7
Cristian Florea <cristianflorea@Cristians-MacBook-Pro.local>
2018-04-17 20:29:05 +0100

    added basic integration test structure

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
-
+const cleanCache = require("../middlewares/cleanCache");
 const Blog = mongoose.model("Blog");
 
 module.exports = app => {
@@ -14,11 +14,14 @@ module.exports = app => {
   });
 
   app.get("/api/blogs", requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
+    console.log(blogs);
     res.send(blogs);
   });
 
-  app.post("/api/blogs", requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
--- services/cache.js
+++ services/cache.js
@@ -3,32 +3,39 @@ const redis = require("redis");
 const util = require("util");
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 const stringify = data => JSON.stringify(data);
 
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = stringify(options.key || "");
+  return this;
+};
+
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
   const key = stringify({
     ...this.getQuery(),
     collection: this.mongooseCollection.name
   });
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
   if (cacheValue) {
     const document = JSON.parse(cacheValue);
-    console.log(document);
     const result = Array.isArray(document)
       ? document.map(doc => new this.model(doc))
       : new this.model(document);
     return result;
-    // return exec.apply(this, arguments);
   }
-
-  // see if we have a value for key
-  // if yes issue it
-
-  // otherwise, store it in redis the issue it
-
   const result = await exec.apply(this, arguments);
-  client.set(key, stringify(result));
+  await client.hset(this.hashKey, key, stringify(result), "EX", 10);
   return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(stringify(hashKey));
+  }
 };
